e9c8ba502a674ef6527de9463a282af04a112517
a_galustyan <a_galustyan@head-point.ru>
2020-10-01 19:05:43 +0300

    Начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app/app';
 
+const MovieInfo = {
+  MOVIE_TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_YEAR: 2014,
+};
+
+ReactDom.render(
+  <App movieTitle={MovieInfo.MOVIE_TITLE} genre={MovieInfo.GENRE} releaseYear={MovieInfo.RELEASE_YEAR} />,
+  document.querySelector(`#root`),
+);
