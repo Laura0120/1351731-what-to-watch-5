e983c280b7b20dd5d7d246f328ef12d5ad251862
a_galustyan <a_galustyan@head-point.ru>
2020-10-05 17:26:42 +0300

    маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MainScreen from '../main-screen/main-screen';
+import Main from '../main/main';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Film from '../film/film';
+import AddReview from '../add-review/add-review';
+import MyList from '../my-list/my-list';
+import Player from '../player/player';
+import SignIn from '../sign-in/sign-in';
 
 const App = (props) => {
   const {movieTitle, genre, releaseYear} = props;
 
-  return <MainScreen movieTitle={movieTitle} genre={genre} releaseYear={releaseYear} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Main} movieTitle={movieTitle} genre={genre} releaseYear={releaseYear} />
+        <Route exact path='/login' component={SignIn} />
+        <Route exact path='/mylist' component={MyList} />
+        <Route exact path='/films/:id' component={Film} />
+        <Route exact path='/films/:id/review' component={AddReview} />
+        <Route exact path='/player/:id' component={Player} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
